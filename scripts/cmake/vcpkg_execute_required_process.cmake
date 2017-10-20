039a3cd2bf888de16bb57c91e776a4ca5b8a7ba3
Robert Schumacher <roschuma@microsoft.com>
2017-10-19 17:19:11 -0700

    [vcpkg-build-cmake] Build at IDLE priority. Try to detect out-of-memory issues in the linker, and restart the build once.

--- scripts/cmake/vcpkg_execute_required_process.cmake
+++ scripts/cmake/vcpkg_execute_required_process.cmake
@@ -30,16 +30,14 @@
 ## * [qt5](https://github.com/Microsoft/vcpkg/blob/master/ports/qt5/portfile.cmake)
 function(vcpkg_execute_required_process)
     cmake_parse_arguments(vcpkg_execute_required_process "" "WORKING_DIRECTORY;LOGNAME" "COMMAND" ${ARGN})
-    #debug_message("vcpkg_execute_required_process(${vcpkg_execute_required_process_COMMAND})")
     execute_process(
         COMMAND ${vcpkg_execute_required_process_COMMAND}
         OUTPUT_FILE ${CURRENT_BUILDTREES_DIR}/${vcpkg_execute_required_process_LOGNAME}-out.log
         ERROR_FILE ${CURRENT_BUILDTREES_DIR}/${vcpkg_execute_required_process_LOGNAME}-err.log
         RESULT_VARIABLE error_code
         WORKING_DIRECTORY ${vcpkg_execute_required_process_WORKING_DIRECTORY})
-    #debug_message("error_code=${error_code}")
-    file(TO_NATIVE_PATH "${CURRENT_BUILDTREES_DIR}" NATIVE_BUILDTREES_DIR)
     if(error_code)
+        file(TO_NATIVE_PATH "${CURRENT_BUILDTREES_DIR}" NATIVE_BUILDTREES_DIR)
         message(FATAL_ERROR
             "  Command failed: ${vcpkg_execute_required_process_COMMAND}\n"
             "  Working Directory: ${vcpkg_execute_required_process_WORKING_DIRECTORY}\n"
